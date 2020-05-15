2ddd1a3d9ff02a344429317505a5a487997ca10c
Alina Kurliantseva <65302512+alina-kurliantseva@users.noreply.github.com>
2020-05-15 07:55:58 -0400

    Add files via upload

---
+++ js/photography-portfolio.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function ($) {
+  $("#alina-yildir-brand").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+  $("#navbar-toggler-demo a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+  $("#navbar-footer a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
